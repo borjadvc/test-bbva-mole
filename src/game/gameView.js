6e8d5559de78b0de283d808ea30ab2047c01fc38
DE VICENTE CASADO Borja <b.devicentecasado@soprasteria.com>
2023-06-06 12:50:13 +0200

    feature(mole): create game logic

--- src/game/gameView.js
+++ src/game/gameView.js
@@ -1,23 +1,110 @@
 import { html, css, LitElement } from 'lit';
 
-const moleImage = new URL(
-  '../../../assets/images/mole-image.png',
-  import.meta.url
-).href;
-
 export class GameView extends LitElement {
-  static properties = {};
+  static styles = css`
+    :host {
+      display: block;
+      text-align: center;
+      --mole-image-url: url('../../../assets/images/mole-image.png');
+    }
 
-  static styles = css``;
+    .cell {
+      display: inline-block;
+      width: 100px;
+      height: 100px;
+      border: 2px solid black;
+      cursor: pointer;
+    }
+
+    .mole {
+      display: none;
+      width: 80px;
+      height: 80px;
+      background-image: var(--mole-image-url);
+      background-size: cover;
+      border-radius: 50%;
+      margin: 10px auto;
+    }
+
+    .show {
+      display: block;
+    }
+
+    .score {
+      font-size: 24px;
+      margin-top: 20px;
+    }
+  `;
+
+  static properties = {
+    score: { type: Number },
+    cells: { type: Array },
+    playing: { type: Boolean },
+    intervalId: { type: Number },
+  };
 
   constructor() {
     super();
-    this.userName = '';
-    this.buttonName = 'join';
+    this.score = 0;
+    this.cells = Array(9).fill(false);
+    this.playing = false;
+    this.intervalId = null;
   }
 
   render() {
-    return html` <section class=""></section> `;
+    return html`
+      <div>
+        ${this.cells.map(
+          (cell, index) => html`
+            <div class="cell" @click="${() => this._handleCellClick(index)}">
+              <div class="mole ${cell ? 'show' : ''}"></div>
+            </div>
+          `
+        )}
+      </div>
+      ${this.playing
+        ? html` <button @click="${this._handleStopClick}">Stop</button> `
+        : html` <button @click="${this._handlePlayClick}">Play</button> `}
+      <div class="score">Score: ${this.score}</div>
+    `;
+  }
+
+  _handlePlayClick() {
+    this.playing = true;
+    this.score = 0;
+    this._startGameLoop();
+  }
+
+  _handleStopClick() {
+    this.playing = false;
+    this._stopGameLoop();
+    this.cells = Array(9).fill(false);
+  }
+
+  _startGameLoop() {
+    this.intervalId = setInterval(() => {
+      this._showRandomMole();
+    }, 1000);
+  }
+
+  _stopGameLoop() {
+    clearInterval(this.intervalId);
+  }
+
+  _showRandomMole() {
+    this.cells = this.cells.map(() => Math.random() < 0.5);
+  }
+
+  _handleCellClick(index) {
+    if (this.cells[index]) {
+      this.score++;
+      alert('¡Felicidades! Has golpeado al topo.');
+    }
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._stopGameLoop();
   }
 }
 window.customElements.define('game-view', GameView);
